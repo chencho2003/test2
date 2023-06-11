51136ff2aadd39aa3fc45cd9d9f357948d6b4cee
chencho2003 <56181309+chencho2003@users.noreply.github.com>
2023-06-11 13:28:10 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // app.use(cors())
 app.use(
   cors({
-    origin: "https://test2-production-b226.up.railway.app/",
+    origin: "https://test2-production-b226.up.railway.app",
     credentials: true,
   })
 );
